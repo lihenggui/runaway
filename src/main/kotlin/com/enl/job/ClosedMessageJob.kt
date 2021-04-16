662c3f47d265c4c8ea70bc85f917bc194094e85f
Mercury <350699171@qq.com>
2021-04-16 23:01:35 +0800

    Refine contents

--- src/main/kotlin/com/enl/job/ClosedMessageJob.kt
+++ src/main/kotlin/com/enl/job/ClosedMessageJob.kt
@@ -6,10 +6,13 @@ import org.quartz.JobExecutionContext
 import java.text.SimpleDateFormat
 import java.util.*
 
-class ClosedMessageJob: Job {
+class ClosedMessageJob : Job {
     override fun execute(context: JobExecutionContext?) {
         val sdf = SimpleDateFormat("yyyy/MM/dd HH:mm:ss", Locale.CHINESE)
         println("Execute ClosedMessageJob at time ${sdf.format(Date())}")
-        FundBot().sendMessage("止跌了\uD83C\uDFC3")
+        FundBot().run {
+            sendMessage("止跌了\uD83C\uDFC3")
+            sendRunAwaySticker()
+        }
     }
 }
