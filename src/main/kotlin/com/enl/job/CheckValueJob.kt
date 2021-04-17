d79d40a71ac70a9f8b25275d5525c6ac01f116e7
Mercury <350699171@qq.com>
2021-04-17 14:27:59 +0800

    Refine check value tasks

---
+++ src/main/kotlin/com/enl/job/CheckValueJob.kt
@@ -0,0 +1,46 @@
+package com.enl.job
+
+import com.enl.FundBot
+import com.enl.day.DayInfo
+import org.quartz.JobExecutionContext
+import java.net.URL
+import java.text.SimpleDateFormat
+import java.util.*
+
+class CheckValueJob : BaseJob() {
+    override fun execute(context: JobExecutionContext?) {
+        super.execute(context)
+        if (!DayInfo.isTradingDay()) {
+            println("Non trading day, skip")
+            return
+        }
+        FundBot().sendMessage(getValueMessage())
+    }
+
+    private fun getValueMessage(): String {
+        val data = URL("http://hq.sinajs.cn/rn=1618638691874&list=s_sh000001,s_sz399001,s_sz399006")
+            .openStream()
+            .bufferedReader()
+            .readLines()
+        val szzsData = getValueAndIncreaseFromString(data[0])
+        val szzsValue = szzsData.first
+        val szzsIncrease = szzsData.second
+        val szczData = getValueAndIncreaseFromString(data[1])
+        val szczValue = szczData.first
+        val szczIncrease = szczData.second
+        val cybzData = getValueAndIncreaseFromString(data[2])
+        val cybzValue = cybzData.first
+        val cybzIncrease = cybzData.second
+        val date = SimpleDateFormat("yyyy年MM月dd日 HH时mm分ss秒").format(Date())
+        return "${date}，沪指报${szzsValue}点，涨跌幅$szzsIncrease%；深证成指报${szczValue}点，涨跌幅$szczIncrease%；创业板指报${cybzValue}点，涨跌幅$cybzIncrease%。"
+    }
+
+    private fun getValueAndIncreaseFromString(data: String): Pair<String, String> {
+        val stockData = data.split("=")
+            .last()
+            .removeSuffix(";")
+            .removeSurrounding("\"")
+            .split(",")
+        return Pair(stockData[1], stockData[3])
+    }
+}
--- src/main/kotlin/com/enl/job/ClosedMessageJob.kt
+++ src/main/kotlin/com/enl/job/ClosedMessageJob.kt
@@ -3,6 +3,7 @@ package com.enl.job
 import com.enl.FundBot
 import com.enl.day.DayInfo
 import org.quartz.JobExecutionContext
+import java.net.URL
 
 class ClosedMessageJob : BaseJob() {
     override fun execute(context: JobExecutionContext?) {
@@ -11,9 +12,30 @@ class ClosedMessageJob : BaseJob() {
             println("Non trading day, skip")
             return
         }
-        FundBot().run {
-            sendMessage("止跌了\uD83C\uDFC3")
-            sendRunAwaySticker()
+        val bot = FundBot()
+        if (isIncreased()) {
+            bot.run {
+                sendMessage("止跌了\uD83C\uDFC3")
+                sendRunAwaySticker()
+            }
+        } else {
+            bot.run {
+                bot.sendMessage("还行。")
+                sendGoodSticker()
+            }
         }
+    }
+
+    private fun isIncreased(): Boolean {
+        val data = URL("http://hq.sinajs.cn/rn=1618638691874&list=s_sh000001")
+            .openStream()
+            .bufferedReader()
+            .readLine()
+        return data.split("=")
+            .last()
+            .removeSuffix(";")
+            .removeSurrounding("\"")
+            .split(",")[3]
+            .toDouble() > 0
     }
 }
